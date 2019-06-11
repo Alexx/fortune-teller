2b0849d695b35bf1f8a8e2f368a91404535afaeb
Alex Siegberg and Micah Zaayer <alex@>
2019-06-11 15:48:27 -0700

    refactor code to seperate front-end from back-end

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
+//back-end
+const luckyThreshold = 2;
+const unluckyThreshold = -2;
+
+/*
+   Name: showLucky
+   Parameters: none
+   Description: hides unlucky and unreadable divs,
+   shows lucky div
+*/
+function showLucky() {
+  $(".unlucky, .unreadable").hide();
+  $(".lucky").show();
+}
+
+/*
+   Name: showUnlucky
+   Parameters: none
+   Description: hides lucky and unreadable divs,
+   shows unlucky div
+*/
+function showUnlucky() {
+  $(".lucky, .unreadable").hide();
+  $(".unlucky").show();
+}
+
+/*
+   Name: showUnreadable
+   Parameters: none
+   Description: hides unlucky and lucky divs,
+   shows unreadable div
+*/
+function showUnreadable() {
+  $(".lucky, .unlucky").hide();
+  $(".unreadable").show();
+}
+
+//front-end
 $(document).ready(function(){
   $(".fortuneForm").submit(function(event) {
     event.preventDefault();
@@ -7,29 +45,26 @@ $(document).ready(function(){
       answeredQuestions.push(backgroundQuestion);
     });
     $("input:checkbox[name=recentHappenings]:checked").each(function() {
-      var backgroundQuestion = parseInt($(this).val());
-      answeredQuestions.push(backgroundQuestion);
+      var recentQuestion = parseInt($(this).val());
+      answeredQuestions.push(recentQuestion);
     });
     $("input:checkbox[name=familyHistory]:checked").each(function() {
-      var backgroundQuestion = parseInt($(this).val());
-      answeredQuestions.push(backgroundQuestion);
+      var familyQuestion = parseInt($(this).val());
+      answeredQuestions.push(familyQuestion);
     });
 
     var totalScore = 0;
-    answeredQuestions.forEach(function(question) {
-      return totalScore += question;
+    answeredQuestions.forEach(function(questionValue) {
+      return totalScore += questionValue;
     });
 
-    if(totalScore <= -2) {
-      $(".lucky, .unreadable").hide();
-      $(".unlucky").show();
-    } else if (totalScore >= 2) {
-      $(".unlucky, .unreadable").hide();
-      $(".lucky").show();
+    if(totalScore <= unluckyThreshold) {
+      showUnlucky();
+    } else if (totalScore >= luckyThreshold) {
+      showLucky();
     } else {
-      $(".lucky, .unlucky").hide();
-      $(".unreadable").show();
-
+      showUnreadable();
     }
+
   });
 });
